69d40d3375c670558b4afb0cb0272bffc139eb3d
Arthur <arthur91021@gmail.com>
2025-02-20 10:44:59 -0800

    useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div className="App"> Hello </div>;
-}
+import DateCounter from "./DateCounter";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
+}
