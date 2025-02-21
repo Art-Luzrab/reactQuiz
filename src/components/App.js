71f59272de71e3b7eb6e966ebdbcba716a10c217
Arthur <arthur91021@gmail.com>
2025-02-21 09:41:27 -0800

    Restarting a quiz

--- src/components/App.js
+++ src/components/App.js
@@ -54,6 +54,14 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...state,
+        status: "ready",
+        index: 0,
+        answer: null,
+        points: 0,
+      };
     default:
       throw new Error("Action unknown");
   }
@@ -113,6 +121,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
